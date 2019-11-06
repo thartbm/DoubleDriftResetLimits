d5c10f4326a806e5668ab258381f73c0d49be606
thartbm <thartbm@gmail.com>
2019-11-06 10:35:56 -0500

    completed functions to download data from OSF

---
+++ R/data.R
@@ -0,0 +1,102 @@
+# check / download data -----
+
+downloadRawData <- function(overwrite=TRUE, experiments=c('bounded_tracking', 'onePass_V4')) {
+  
+  
+  for (experiment in experiments) {
+    
+    if (experiment == 'bounded_tracking') {
+      
+      folder = 'data/bounded_tracking'
+      
+      if (!dir.exists(folder)) {
+        dir.create(folder)
+      }
+      
+      resources <- c( 
+        
+        'bounded_tracking_p01.csv' = 'https://osf.io/xrbp7/download',
+        'bounded_tracking_p03.csv' = 'https://osf.io/2p3nc/download',
+        'bounded_tracking_p05.csv' = 'https://osf.io/trzdx/download',
+        'bounded_tracking_p06.csv' = 'https://osf.io/qywdc/download',
+        'bounded_tracking_p07.csv' = 'https://osf.io/59pcd/download',
+        'bounded_tracking_p08.csv' = 'https://osf.io/sfjc6/download'
+        
+      )
+      
+      downloadFiles(files=resources, folder=folder, overwrite=overwrite)
+      
+      next()
+      
+    }
+    
+    if (experiment == 'onePass_V4') {
+      
+      folder = 'data/onePass_V4'
+      
+      if (!dir.exists(folder)) {
+        dir.create(folder)
+      }
+      
+      # participants 1 and 7 are not used
+      # I am participant 1 (I think),
+      # participant 7 had the opposite percept of what was expected
+      # (despite doing the bounded tracking task too, and having normal percepts there)
+      
+      resources <- c( 
+        
+        'onepass_V4_p01.csv' = 'https://osf.io/cnbxm/download',
+        'onepass_V4_p02.csv' = 'https://osf.io/uvz4y/download',
+        'onepass_V4_p03.csv' = 'https://osf.io/zpckg/download',
+        'onepass_V4_p04.csv' = 'https://osf.io/5wkp7/download',
+        'onepass_V4_p05.csv' = 'https://osf.io/6u85g/download',
+        'onepass_V4_p06.csv' = 'https://osf.io/bvqt4/download',
+        'onepass_V4_p07.csv' = 'https://osf.io/a6e43/download',
+        'onepass_V4_p08.csv' = 'https://osf.io/vpw3n/download',
+        'onepass_V4_p09.csv' = 'https://osf.io/tm3c8/download',
+        'onepass_V4_p10.csv' = 'https://osf.io/8u9rb/download',
+        'onepass_V4_p11.csv' = 'https://osf.io/v4d8p/download'
+        
+      )
+      
+      downloadFiles(files=resources, folder=folder, overwrite=overwrite)
+      
+      next()
+      
+    }
+    
+    cat(sprintf('Not a recognized experiment: %s\n',experiment))
+    
+  }
+  
+}
+
+
+downloadFiles <- function(files, folder='./', overwrite=TRUE) {
+  
+  for (filename in names(files)) {
+    
+    filepath <- sprintf('%s/%s',folder,filename)
+    
+    if (overwrite | !file.exists(filepath)) {
+      
+      url = as.character(files[filename])
+      
+      cat(sprintf("Downloading: '%s' from '%s'\n", filename, url))
+      
+      download.file(url = url, 
+                    destfile = filepath, 
+                    method = 'auto', 
+                    quiet = FALSE, 
+                    mode = "wb")
+      
+      
+    } else {
+      
+      cat(sprintf("File exists, not downloading: '%s'\n", filepath))
+      
+    }
+    
+  }
+  
+}
