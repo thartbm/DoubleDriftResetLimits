220a2a522df4d63990b800477d22f690643cbc9c
thartbm <thartbm@gmail.com>
2021-11-10 10:40:56 -0500

    save a series of actual stimuli to calculate luminance of gabors

--- exp/frameGrab.py
+++ exp/frameGrab.py
@@ -1,31 +1,34 @@
+import numpy as np
 import scipy as sp
 from setup import *
 
 # this function is meant to create a high-resolution gabor to illustrate what we do:
 
-def createExampleGabor():
+def createExampleGabors():
   
   # use stuff from setup file as much as possible:
   cfg = {}
   cfg['flip'] = False
-  cfg['fullscr'] = True
+  cfg['fullscr'] = False
   
   # create fullscreen, upside up window:
-  cfg = createWindow(cfg)
+  cfg = createWindow(cfg, resolution=[100,100])
   cfg = createStimuli(cfg)
   
   # change some settings to increase the resolution:
-  cfg['gabor'].size = 1000
-  cfg['gabor'].sf = 1.5/1000
-  cfg['gabor'].phase = (0.80, 0.0)
+  cfg['gabor'].size = 100
+  cfg['gabor'].sf = 1.5/100
   
-  # put on the screen:
-  cfg['gabor'].draw()
-  cfg['win'].flip()
+  for phase in np.arange(0,1,45/360):
+    cfg['gabor'].phase = (phase, 0.0)
   
-  # grab the current frame, and save it to a file:
-  cfg['win'].getMovieFrame()
-  cfg['win'].saveMovieFrames('gabor1000.png')
+    # put on the screen:
+    cfg['gabor'].draw()
+    cfg['win'].flip()
+  
+    # grab the current frame, and save it to a file:
+    cfg['win'].getMovieFrame()
+    cfg['win'].saveMovieFrames('gabor_%d.png'%(int(phase*360)))
   
   # release window:
   cfg['win'].close()
